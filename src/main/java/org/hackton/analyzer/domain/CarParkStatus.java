f1a377e3d0f333f5ab00b7ff65e30c31f6220117
dfa19 <dare.famuyiwa@sky.uk>
2019-07-26 13:31:41 +0100

    Added basic files

---
+++ src/main/java/org/hackton/analyzer/domain/CarParkStatus.java
@@ -0,0 +1,11 @@
+package org.hackton.analyzer.domain;
+
+public class CarParkStatus {
+    /*
+      0 = General
+      1 = Shift
+      2 = Reserved
+     */
+    private String carParkTypeId;
+    private String count;
+}
