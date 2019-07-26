80d31df27f69f4cdc6a8125d2935e3d2823e87f6
dfa19 <dare.famuyiwa@sky.uk>
2019-07-26 13:35:11 +0100

    Added basic files

--- src/main/java/org/hackton/analyzer/domain/CarParkStatus.java
+++ src/main/java/org/hackton/analyzer/domain/CarParkStatus.java
@@ -1,11 +1,14 @@
 package org.hackton.analyzer.domain;
 
 public class CarParkStatus {
-    /*
+    /**
       0 = General
       1 = Shift
       2 = Reserved
      */
     private String carParkTypeId;
+    /**
+     * count = FULL (if used == total) else count = used
+     */
     private String count;
 }
